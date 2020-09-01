a54a1be150d37b26b56a7ad4d36851b5d9b40e35
vysahjk <vysahjk@gmail.com>
2020-09-01 13:42:51 +0200

    test azure redirection english

---
+++ src/.vuepress/public/scripts/redirection.js
@@ -0,0 +1,7 @@
+if (navigator.language === 'en-US') {
+
+  if (window.location.href === `https://kind-desert-00b6b8203.azurestaticapps.net/`) {
+    window.location.href = "https://kind-desert-00b6b8203.azurestaticapps.net/en/"
+  }
+
+}
