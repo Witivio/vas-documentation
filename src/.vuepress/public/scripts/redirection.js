727942bb95726f7f2235dfc173d2032592786fdd
vysahjk <vysahjk@gmail.com>
2020-09-01 16:33:53 +0200

    ajoute redirection vers docs.witivio.com/en/

--- src/.vuepress/public/scripts/redirection.js
+++ src/.vuepress/public/scripts/redirection.js
@@ -1,7 +1,13 @@
+
+//redirection navigator en-US
 if (navigator.language === 'en-US') {
 
   if (window.location.href === `https://kind-desert-00b6b8203.azurestaticapps.net/`) {
     window.location.href = "https://kind-desert-00b6b8203.azurestaticapps.net/en/"
+  }
+
+  if (window.location.href === `https://docs.witivio.com/`) {
+    window.location.href = "https://docs.witivio.com/en/"
   }
 
 }
