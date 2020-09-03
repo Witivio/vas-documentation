cec945f7d9c7422633eea069b09434ec4aee1a0d
vysahjk <vysahjk@gmail.com>
2020-09-03 17:53:57 +0200

    ajoute fichier routes.json en public

--- src/.vuepress/public/scripts/redirection.js
+++ src/.vuepress/public/scripts/redirection.js
@@ -2,13 +2,18 @@
 
 if (navigator.language === 'fr-FR' || navigator.language === 'fr' ) {
 
-  if (window.location.href === 'https://docs.witivio.com/') {
+  if (window.location.href === "https://docs.witivio.com/") {
     window.location.href = "https://docs.witivio.com/fr/"
   }
 
 } else {
+
+  console.log(navigator.language)
+  console.log(window.location.href)
   
-  if (window.location.href === 'https://docs.witivio.com/') {
+  if (window.location.href === "https://docs.witivio.com/") {
     window.location.href = "https://docs.witivio.com/en/"
   }
+
 }
+
