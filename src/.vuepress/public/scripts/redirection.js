6e21c0938b1635099823b54d4da202af721af6fd
vysahjk <vysahjk@gmail.com>
2020-09-03 18:39:36 +0200

    redirection function replace

--- src/.vuepress/public/scripts/redirection.js
+++ src/.vuepress/public/scripts/redirection.js
@@ -3,16 +3,13 @@
 if (navigator.language === 'fr-FR' || navigator.language === 'fr' ) {
 
   if (window.location.href === "https://docs.witivio.com/") {
-    window.location.href = "https://docs.witivio.com/fr/"
+    window.location.replace("https://docs.witivio.com/fr/");
   }
 
 } else {
-
-  console.log(navigator.language)
-  console.log(window.location.href)
   
   if (window.location.href === "https://docs.witivio.com/") {
-    window.location.href = "https://docs.witivio.com/en/"
+    window.location.replace("https://docs.witivio.com/en/");
   }
 
 }
