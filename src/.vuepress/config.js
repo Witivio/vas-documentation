eaed9a0d48afe87fae30e78b0656ecc67054c638
vysahjk <vysahjk@gmail.com>
2020-09-23 16:53:01 +0200

    modification metabot

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -55,54 +55,6 @@ module.exports = {
     editLinkText: '',
     lastUpdated: false,
     locales: {
-      '/fr/': {
-        searchPlaceholder: 'recherche...',
-        // text for the language dropdown
-        selectText: 'Languages',
-        // label for this locale in the language dropdown
-        label: 'fr-FR',
-        // Aria Label for locale in the dropdown
-        ariaLabel: 'Languages',
-        // text for the edit-on-github link
-        editLinkText: 'Edit this page on GitHub',
-        // config for Service Worker
-        serviceWorker: {
-          updatePopup: {
-            message: "New content is available.",
-            buttonText: "Refresh"
-          }
-        },
-        // algolia docsearch options for current locale
-        algolia: {},
-        nav: [
-          { text: 'Witivio 365', link: '/fr/chatbot/creation_chatbot' },
-          { text: 'Dynameet', link: '/fr/dynameet/' },
-          { text: 'Videos', link: '/fr/videos/' },
-          { text: 'Teams Apps', link: '/fr/teams_apps/' },
-          { text: 'Trust Center', link: '/en/trust-center/'}
-        ]
-      },
-      '/en/': {
-        searchPlaceholder: 'search...',
-        selectText: 'Languages',
-        label: 'en-US',
-        editLinkText: 'text GitHub witivio',
-        serviceWorker: {
-          updatePopup: {
-            message: "New content is available..",
-            buttonText: "Refresh"
-          }
-        },
-        nav: [
-          { text: 'Witivio 365', link: '/en/chatbot/build_your_chatbot' },
-          { text: 'Dynameet', link: '/en/dynameet/' },
-          { text: 'Videos', link: '/en/videos/' },
-          { text: 'Teams Apps', link: '/en/teams_apps/' },
-          { text: 'Trust Center', link: '/en/trust-center/' }
-
-        ],
-        algolia: {}
-      },
         '/': {
             searchPlaceholder: 'search...',
             selectText: 'Languages',
@@ -123,7 +75,55 @@ module.exports = {
 
             ],
             algolia: {}
-        }
+        },
+        '/en/': {
+            searchPlaceholder: 'search...',
+            selectText: 'Languages',
+            label: 'en-US',
+            editLinkText: 'text GitHub witivio',
+            serviceWorker: {
+            updatePopup: {
+            message: "New content is available..",
+            buttonText: "Refresh"
+          }
+        },
+        nav: [
+          { text: 'Witivio 365', link: '/en/chatbot/build_your_chatbot' },
+          { text: 'Dynameet', link: '/en/dynameet/' },
+          { text: 'Videos', link: '/en/videos/' },
+          { text: 'Teams Apps', link: '/en/teams_apps/' },
+          { text: 'Trust Center', link: '/en/trust-center/' }
+
+        ],
+        algolia: {}
+      },
+    '/fr/': {
+        searchPlaceholder: 'recherche...',
+        // text for the language dropdown
+        selectText: 'Languages',
+        // label for this locale in the language dropdown
+        label: 'fr-FR',
+        // Aria Label for locale in the dropdown
+        ariaLabel: 'Languages',
+        // text for the edit-on-github link
+        editLinkText: 'Edit this page on GitHub',
+        // config for Service Worker
+        serviceWorker: {
+            updatePopup: {
+                message: "New content is available.",
+                buttonText: "Refresh"
+            }
+        },
+        // algolia docsearch options for current locale
+        algolia: {},
+        nav: [
+            { text: 'Witivio 365', link: '/fr/chatbot/creation_chatbot' },
+            { text: 'Dynameet', link: '/fr/dynameet/' },
+            { text: 'Videos', link: '/fr/videos/' },
+            { text: 'Teams Apps', link: '/fr/teams_apps/' },
+            { text: 'Trust Center', link: '/en/trust-center/' }
+        ]
+    }
     },
     sidebar: {
       '/en/chatbot/': [
@@ -268,7 +268,7 @@ module.exports = {
           // ]
         },
         {
-            title: 'Metabot',
+            title: 'MetaBot',
             path: '/en/chatbot/metabot',
             collapsable: true,
             // children: [
@@ -418,7 +418,7 @@ module.exports = {
           // ]
         },
         {
-            title: 'Metabot',
+            title: 'MetaBot',
             path: '/fr/chatbot/metabot',
             collapsable: true,
             children: [
@@ -568,7 +568,7 @@ module.exports = {
           // ]
         },
         {
-            title: 'Metabot',
+            title: 'MetaBot',
             path: '/fr/chatbot/metabot',
             collapsable: true,
             // children: [
